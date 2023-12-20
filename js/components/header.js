b6c58c14a69766f8a6fa840b6f6319944edd9227
Louis-quentin.lecoq@avisto.com llecoq <lecoqlq@gmail.com>
2023-12-20 15:58:47 +0100

    js scripts refacto

---
+++ js/components/header.js
@@ -0,0 +1,46 @@
+/*--------------------------------------------------------------- Navbar */
+
+export function initNavbar() {
+
+    /* Active element */
+    let lastActiveElement = null;
+    
+    window.addEventListener('scroll', function() {
+        const sections = document.querySelectorAll('section');
+        const navLinks = document.querySelectorAll('#nav-links a');
+        
+        let currentSection = '';
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (this.scrollY >= (sectionTop - sectionHeight / 3)) {
+                currentSection = section.getAttribute('id');
+            }
+        });
+        
+        navLinks.forEach(link => { 
+            link.classList.remove('active');
+            if (link.getAttribute('href') === '#' + currentSection) {
+                link.classList.add('active');
+                
+                lastActiveElement = link;
+            }
+        });
+    });
+    
+    document.querySelectorAll('#nav-links a').forEach(link => {
+        link.addEventListener('mouseover', function() {
+            document.querySelectorAll('#nav-links a').forEach(otherLink => {
+                otherLink.classList.remove('active');
+            });
+        });
+    });
+    
+    document.querySelectorAll('#nav-links a').forEach(link => {
+        link.addEventListener('mouseleave', function() {
+            if (lastActiveElement) {
+                lastActiveElement.classList.add('active');
+            }
+        });
+    });
+}
