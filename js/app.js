ba282c871b2d3adc10beb859124863e8fb3a133a
llecoq <lecoqlq@gmail.com>
2023-12-20 17:17:06 +0100

    Implement particles

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 import { initNavbar } from "./components/header.js";
 import { initHome, adjustHomeSize } from "./sections/home.js";
+import { initParticles } from "./particles.js";
 
 initNavbar();
 initHome();
 adjustHomeSize();
+initParticles();
+// connectParticles();
