ce407d6b047d5d81701f0540e59b874a09f5d202
llecoq <llecoq@student.42lyon.fr>
2023-12-23 11:40:16 +0100

    refacto particles

--- js/components/particles/objs/Impulse.js
+++ js/components/particles/objs/Impulse.js
@@ -10,6 +10,7 @@ export default class Impulse {
     speed;
     distAutonomy;
 
+    // Constructor
     constructor(x, y, particle, neighbor) {
         this.x = x;
         this.y = y;
@@ -20,32 +21,28 @@ export default class Impulse {
     }
 
     // Update impulse position
-    updateImpulsePosition() {
-        let dx = this.target.x - this.x;
-        let dy = this.target.y - this.y;
-        let length = Math.sqrt(dx * dx + dy * dy);
-        dx /= length;
-        dy /= length;
+    updateImpulsePosition(steps = 8) {
+        for (let i = 0; i < steps; i++) {
+            let dx = this.target.x - this.x;
+            let dy = this.target.y - this.y;
+            let length = Math.sqrt(dx * dx + dy * dy);
+            dx /= length;
+            dy /= length;
 
-        this.x += dx * this.speed;
-        this.y += dy * this.speed;
-        this.distAutonomy -= 10;
+            this.x += dx * this.speed;
+            this.y += dy * this.speed;
+        }
+
+        this.distAutonomy -= this.speed * steps;
     }
 
     // Draw impulse
     draw(ctx) {
-        this.updateImpulsePosition();
+        this.updateImpulsePosition(1);
         ctx.beginPath();
         ctx.strokeStyle = 'rgb(72, 217, 247)';
         ctx.lineWidth = opts.IMPULSE_SIZE;
         ctx.moveTo( this.x, this.y );
-        this.updateImpulsePosition();
-        this.updateImpulsePosition();
-        this.updateImpulsePosition();
-        this.updateImpulsePosition();
-        this.updateImpulsePosition();
-        this.updateImpulsePosition();
-        this.updateImpulsePosition();
         this.updateImpulsePosition();
         ctx.lineTo( this.x, this.y );
         ctx.stroke();
@@ -54,5 +51,24 @@ export default class Impulse {
     // Get next neighbor
     getNextNeighbor(origin, particle) {
         return particle.neighbors.find(neighbor => getDist(particle.x, particle.y, neighbor.x, neighbor.y) < opts.MAX_DIST && neighbor !== origin && !neighbor.active);
+    }
+
+    // Returns true if the `Impulse` is expired
+    isExpired() {
+        return this.distAutonomy <= 0 && !this.target
+    }
+
+    // Move to next target
+    move() {
+        if (getDist(this.x, this.y, this.target.x, this.target.y) < 10) {
+            const nextTarget = this.getNextNeighbor(this.particle, this.target);
+            if (nextTarget) {
+                this.particle = this.target;
+                this.target = nextTarget;
+                nextTarget.activateTimer();
+                return true;
+            }
+            return false;
+        }
     }
 }
