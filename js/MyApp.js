be9cb8509d79b1d1760e4ff018cba8e9278f23f6
llecoq <lecoqlq@gmail.com>
2023-12-30 14:51:47 +0100

    refacto

--- js/MyApp.js
+++ js/MyApp.js
@@ -1,36 +1,19 @@
 import { initNavbar } from "./components/header.js";
 import { initHome, adjustHomeSize } from "./sections/home.js";
-import { particles } from "./components/particles/particles.js";
+import { Particles } from "./components/particles/particles.js";
 
 export default class MyApp {
 
-    particlesWASM
-    particlesJS
-    useWASM
+    particles
 
     constructor() {
-        this.useWASM = true;
-        document.getElementById("animation-toggle-switch").addEventListener("change", this.toggleAnimation.bind(this));
-    }
-    
-    toggleAnimation() {
-        if (this.useWASM === true) {
-            this.useWASM = false;
-            // this.wasmAnimation.stop();
-            // this.jsAnimation.start();
-            console.log('JS anim')
-        } else {
-            this.useWASM = true;
-            // this.jsAnimation.stop();
-            // this.wasmAnimation.start();
-            console.log('WASM anim')
-        }
+        this.particles = new Particles();
     }
     
     start() {
         initNavbar();
         initHome();
         adjustHomeSize();
-        particles();
+        this.particles.init();
     }
 }
