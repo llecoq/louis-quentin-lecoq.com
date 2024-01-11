9521a45497ca25c4a981da13f38fc12b75487045
llecoq <lecoqlq@gmail.com>
2024-01-11 14:42:42 +0100

    get_opts from JS to Rust

--- src/particles/particles_manager.rs
+++ src/particles/particles_manager.rs
@@ -1,12 +1,20 @@
+use wasm_bindgen::prelude::*;
+use gloo_console::log;
+
+use crate::particles::get_opts;
+
+use super::particle::Particle;
+
 #[wasm_bindgen]
 pub struct ParticlesManagerWASM {
     particles: Vec<Particle>,
 }
 
+#[wasm_bindgen]
 impl ParticlesManagerWASM {
-
-    pub fn init() {
-
+    pub fn new() -> ParticlesManagerWASM {
+        ParticlesManagerWASM {
+            particles: Vec::new(),
+        }
     }
-
 }
