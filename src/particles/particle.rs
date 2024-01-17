19b707539eb94ed6b8e43d62b84aa918f2dc398f
llecoq <lecoqlq@gmail.com>
2024-01-17 17:17:11 +0100

    flattened Particle Struct in f32

--- src/particles/particle.rs
+++ src/particles/particle.rs
@@ -1,26 +1,30 @@
 use std::f32::consts::PI;
 
 use rand::Rng;
-use wasm_bindgen::prelude::*;
 
-#[derive(Debug)]
-pub struct Color {
-    red: u8,
-    green: u8,
-    blue: u8
-}
-
-#[wasm_bindgen]
+// Not ideal but the Structure needs to be "flatten": every element needs to be of the 
+// same type (here `f32`) to be able to read it as a `Float32Array` on the JS side
 #[derive(Debug)]
 pub struct Particle {
     pub x: f32,
-    y: f32,
-    speed_x: f32,
-    speed_y: f32,
-    size: f32,
-    color: Color,
-    active: bool,
-    neighbors: Vec<usize>,
+    pub y: f32,
+    pub speed_x: f32,
+    pub speed_y: f32,
+    pub size: f32,
+    pub color_red: f32,
+    pub color_green: f32,
+    pub color_blue: f32,
+    pub active: f32,
+    pub neighbor_1: f32,
+    pub neighbor_2: f32,
+    pub neighbor_3: f32,
+    pub neighbor_4: f32,
+    pub neighbor_5: f32,
+    pub neighbor_6: f32,
+    pub neighbor_7: f32,
+    pub neighbor_8: f32,
+    pub neighbor_9: f32,
+    pub neighbor_10: f32,
 }
 
 impl Particle {
@@ -36,40 +40,61 @@ impl Particle {
         let speed_y: f32 = rng.gen();
         let dir_x = if rng.gen::<f32>() > 0.5 {1.0} else {-1.0};
         let dir_y = if rng.gen::<f32>() > 0.5 {1.0} else {-1.0};
+        let chance: f32 = rand::thread_rng().gen();
+
 
         Particle {
             x : center_x as f32 + radius_x * f32::cos(theta),
             y : center_y as f32 + radius_y * f32::sin(theta),
             size : rng.gen::<f32>() * (particle_max_size - particle_min_size) + particle_min_size,
-            color : get_random_particle_color(),
+            color_red : get_random_red_value(chance),
+            color_green : get_random_green_value(chance),
+            color_blue : get_random_blue_value(chance),
             speed_x : speed_x * dir_x,
             speed_y : speed_y * dir_y,
-            active : false,
-            neighbors : Vec::new()
+            active : 0.0,
+            neighbor_1: 0.0,
+            neighbor_2: 0.0,
+            neighbor_3: 0.0,
+            neighbor_4: 0.0,
+            neighbor_5: 0.0,
+            neighbor_6: 0.0,
+            neighbor_7: 0.0,
+            neighbor_8: 0.0,
+            neighbor_9: 0.0,
+            neighbor_10: 0.0,
         }
     }
 }
 
 // Returns RGB values for the color of each particle
-fn get_random_particle_color() -> Color {
-    let chance: f32 = rand::thread_rng().gen();
+fn get_random_red_value(chance: f32) -> f32 {
     if chance < 0.70 {
-        Color {
-            red: 255,
-            blue: 255,
-            green: 255
-        }
+        255.0
     } else if chance < 0.9 {
-        Color {
-            red: 72,
-            blue: 217,
-            green: 247
-        }
+        72.0
     } else {
-        Color {
-            red: 50,
-            blue: 130,
-            green: 240
-        }
+        50.0
     }
 }
+
+fn get_random_green_value(chance: f32) -> f32 {
+    if chance < 0.70 {
+        255.0
+    } else if chance < 0.9 {
+        247.0
+    } else {
+        240.0
+    }
+}
+
+fn get_random_blue_value(chance: f32) -> f32 {
+    if chance < 0.70 {
+        255.0
+    } else if chance < 0.9 {
+        217.0
+    } else {
+        130.0
+    }
+}
+
