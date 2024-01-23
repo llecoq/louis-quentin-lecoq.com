def7d3b65235bb42235967d5c172530ee4310885
Louis-Quentin Lecoq <louis-quentin.lecoq@avisto.com>
2024-01-23 11:56:14 +0100

    Refacto ParticlesManagerWASM

--- src/particles/impulses_manager.rs
+++ src/particles/impulses_manager.rs
@@ -1,8 +1,8 @@
-use std::rc::Rc;
+use std::{rc::Rc, cell::RefCell};
 
 use super::{impulse::Impulse, particle::Particle};
 
 pub struct ImpulsesManagerWASM {
     impulses: Vec<Impulse>,
-    particles: Rc<Vec<Particle>>,
+    particles: Rc<RefCell<Vec<Particle>>>,
 }
--- src/particles/particles_manager.rs
+++ src/particles/particles_manager.rs
@@ -53,21 +53,28 @@ impl ParticlesManagerWASM {
 
     // Initialize particles, following the opts from the JS side
     pub fn init(&mut self) {
-        let number_of_particles = self.opts.number_of_particles;
-
-        for _ in 0..number_of_particles {
-            self.particles.borrow_mut().push(
-                Particle::new(
-                    self.canvas.height, 
-                    self.canvas.width, 
-                    self.opts.particle_min_size, 
-                    self.opts.particle_max_size
-                )
-            );
+        {
+            let number_of_particles = self.opts.number_of_particles;
+            let mut particles = self.particles.borrow_mut();
+            
+            for _ in 0..number_of_particles {
+                particles.push(
+                    Particle::new(
+                        self.canvas.height, 
+                        self.canvas.width, 
+                        self.opts.particle_min_size, 
+                        self.opts.particle_max_size
+                    )
+                );
+            }
+            
+            // Sort particles from smallest to biggest
+            particles.sort_unstable_by(|a, b| {
+                a.size
+                .partial_cmp(&b.size)
+                .expect("Sort particles failed")
+            });
         }
-
-        // Sort particles from smallest to biggest
-        self.particles.borrow_mut().sort_unstable_by(|a, b| a.size.partial_cmp(&b.size).unwrap());
 
         // Sort neighbors from closest to farthest
         self.sort_neighbors();
@@ -80,7 +87,9 @@ impl ParticlesManagerWASM {
 
     // Update positions of each particles
     pub fn update(&mut self, scale_fps: f32) {
-        for elem in self.particles.borrow_mut().iter_mut() {
+        let mut particles = self.particles.borrow_mut();
+
+        for elem in particles.iter_mut() {
             elem.update_position(self.canvas.height, self.canvas.width, scale_fps);
         }
     }
@@ -92,22 +101,30 @@ impl ParticlesManagerWASM {
 
         // Sort distances for each `Particle` in the `neighbors_matrix`
         for i in 0..self.opts.number_of_particles {
-            self.neighbors_matrix[i].sort_unstable_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+            self.neighbors_matrix[i].sort_unstable_by(|a, b| {
+                a.1
+                    .partial_cmp(&b.1)
+                    .expect("Sort ditance for each `Particle` in the neighbors_matrix failed.")
+            });
        
             // Set 10 first neighbors for each `Particle`
+            let mut particles = self.particles.borrow_mut();
+
             for j in 0..10 {
-                self.particles.borrow_mut()[i].set_neighbor(j + 1, self.neighbors_matrix[i][j + 1].0);
+                particles[i].set_neighbor(j + 1, self.neighbors_matrix[i][j + 1].0);
             }
         }
     }
 
     // Update distance between each `Particle` in the `neighbors_matrix`
     fn update_matrix_neighbors_distances(&mut self) {
+        let particles = self.particles.borrow();
+
         for i in 0..self.opts.number_of_particles {
             for j in 0..self.opts.number_of_particles {
-                    self.neighbors_matrix[i][j].0 = j;
-                    let distance =  self.particles.borrow()[i].get_distance_from(&self.particles.borrow()[j]);
-                    self.neighbors_matrix[i][j].1 = distance;
+                self.neighbors_matrix[i][j].0 = j;
+                let distance =  particles[i].get_distance_from(&particles[j]);
+                self.neighbors_matrix[i][j].1 = distance;
             }
         }
     }
