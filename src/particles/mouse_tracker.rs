341979f1cfae4d247e089cd5837dff90356bb52a
Louis-Quentin Lecoq <louis-quentin.lecoq@avisto.com>
2024-01-23 16:38:23 +0100

    Add MousePosition struct

--- src/particles/mouse_tracker.rs
+++ src/particles/mouse_tracker.rs
@@ -1,19 +1,25 @@
 use std::{rc::Rc, cell::RefCell};
 
+pub struct MouseData {
+    pos_x: u32,
+    pos_y: u32,
+    mouse_is_over_canvas: u32,
+}
+
 #[derive(Clone)]
 pub struct MouseTracker {
-    mouse_position: Rc<RefCell<Vec<u32>>>
+    mouse_position: Rc<RefCell<Vec<MouseData>>>
 }
 
 impl MouseTracker {
 
-    pub fn get_mouse_position_ptr(&self) -> *const u32 {
+    pub fn get_mouse_position_ptr(&self) -> *const MouseData {
         self.mouse_position.borrow().as_ptr()
     }
 
     pub fn new() -> MouseTracker {
         MouseTracker {
-            mouse_position: Rc::new(RefCell::new(vec![0, 0]))
+            mouse_position: Rc::new(RefCell::new(vec![]))
         }
     }
 }
